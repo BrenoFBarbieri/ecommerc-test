8f73ed47697aa47327da281e1a437368c3151493
Breno Barbieri <breno.barbieri@kumulus.com.br>
2022-01-19 11:48:13 -0300

    Presentation abstraction for component

---
+++ components/CoverPage.jsx
@@ -0,0 +1,14 @@
+import styles from './coverPage.module.css'
+
+const CoverPage = () => {
+    return (
+        <div className={styles.containerTitle}>
+            <h1>
+            Olá, bem-vindo ao Virgo!
+            </h1>
+            <span className={styles.subTitleAnimation}>Agradecemos sua preferência : )</span>
+        </div>
+    )
+}
+
+export default CoverPage
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,18 +3,15 @@ import Head from 'next/head'
 // import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+import CoverPage from '../components/CoverPage'
+
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
         <title>Virgo - eCommerce</title>
       </Head>
-      <div className={styles.containerTitle}>
-        <h1>
-          Olá, bem-vindo ao Virgo!
-        </h1>
-        <span className={styles.subTitleAnimation}>Agradecemos sua preferência : )</span>
-      </div>
+      <CoverPage />
     </div>
   )
 }
